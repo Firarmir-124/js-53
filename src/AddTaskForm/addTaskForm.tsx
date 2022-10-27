eeb8f2e2384e808a1ec77a5d1c80b2947e42cb52
Polushin Dima <dima2004polushin2004@gmail.com>
2022-10-27 13:36:11 +0600

    Внес изменения в форму

--- src/AddTaskForm/addTaskForm.tsx
+++ src/AddTaskForm/addTaskForm.tsx
@@ -3,16 +3,16 @@ import React from 'react';
 interface typeProps extends React.PropsWithChildren  {
   name: string;
   onNameChange: React.ChangeEventHandler<HTMLInputElement>;
-  add: React.MouseEventHandler;
+  add: React.MouseEventHandler<HTMLFormElement>;
 }
 
 const AddTaskForm:React.FC<typeProps> = props => {
 
   return (
     <div>
-      <form action="#" className='form'>
-        <input type="text" value={props.name} onChange={props.onNameChange}/>
-        <button className='button' onClick={props.add}>Add</button>
+      <form action="#" className='form' onSubmit={props.add}>
+        <input type="text" value={props.name} onChange={props.onNameChange} placeholder='Введите задачу'/>
+        <button type='submit' className='button'>Add</button>
       </form>
     </div>
   );
